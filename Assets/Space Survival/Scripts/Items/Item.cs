feafb31d4671a84168bebc290f9ab36ef59cf372
JiwonEE2 <jiwonchichi1@gmail.com>
2024-10-23 16:44:07 +0900

    241023 16:44 아이템을 습득했을때에도 파티클 재생 되도록 하기

--- Assets/Space Survival/Scripts/Items/Item.cs
+++ Assets/Space Survival/Scripts/Items/Item.cs
@@ -6,8 +6,15 @@ public abstract class Item : MonoBehaviour, IContactable
 {
 	// virtual : 부모에서도 구현해야 함
 	// abstract : 자식에서만 구현해야 함
+
+	public ParticleSystem contactParticle;
+
 	public virtual void Contact()
 	{
+		// 파티클 구현하기
+		var particle = Instantiate(contactParticle, transform.position, Quaternion.identity);
+		particle.Play();
+		Destroy(particle.gameObject, 2f);
 		Destroy(gameObject);
 	}
 }
